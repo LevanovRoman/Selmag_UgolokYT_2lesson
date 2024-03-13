bbe588bd95eace06b30d47743fae24d7888ebb30
levanovroman <levanovroman2016@yandex.ru>
2024-03-13 14:59:47 +0300

    DB

--- catalogue-service/src/main/java/ag/selm/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ag/selm/catalogue/repository/ProductRepository.java
@@ -2,16 +2,28 @@ package ag.selm.catalogue.repository;
 
 
 import ag.selm.catalogue.entity.Product;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductRepository {
-    List<Product> findAll();
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+//    написание собственных запросов - фильтрация по названию
 
-    Product save(Product product);
+//    первый способ
+    // аналог select * from catalogue.t_product where c_title ilike :filter
+//    Iterable<Product> findAllByTitleLikeIgnoreCase(String filter);
 
-    Optional<Product> findById(Integer productId);
+//    второй способ jpql-запрос
+//    @Query(value = "select p from Product p where p.title ilike :filter")
+//    Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
 
-    void deleteById(Integer id);
+    //третий способ нативный запрос на sql
+//    @Query(value = "select * from catalogut.t_product where c_title ilike :filter", nativeQuery = true)
+//    Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
+//    четвертый способ через класс Product
+@Query(name = "Product.findAllByTitleLikeIgnoringCase", nativeQuery = true)
+Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
 }
